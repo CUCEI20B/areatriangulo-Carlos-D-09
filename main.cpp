b39712d02a5792d16facc9aef1bb33db4b8d9bea
Carlos Daniel Medina Sahagun <carlos.medina1311@alumnos.udg.mx>
2020-09-30 17:47:17 -0500

    Revert "Quite los mensajes al usuario", lo reverti debido a que las pruebas en github no lo daban como correcto, pero el anterior si
    This reverts commit 4d347a9ada736c42760ac01e9b2a12686108fdc2.

--- main.cpp
+++ main.cpp
@@ -4,11 +4,16 @@ using namespace std;
 
 int main() 
 {
+    
     unsigned int base;
     unsigned int altura;
+
+    cout <<endl <<"Introduce la base del rectangulo: ";
     cin >> base;
+    cout <<endl <<"Introduce la altura del rectangulo: ";
     cin >> altura;
-    cout<<base*altura;
+
+    cout <<endl<<"El area es: "<<(base * altura)/2<<endl;
 
     return 0;
 }
