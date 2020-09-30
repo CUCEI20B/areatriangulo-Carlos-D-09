4d347a9ada736c42760ac01e9b2a12686108fdc2
Carlos Daniel Medina Sahagun <carlos.medina1311@alumnos.udg.mx>
2020-09-30 17:36:23 -0500

    Quite los mensajes al usuario

--- main.cpp
+++ main.cpp
@@ -4,16 +4,11 @@ using namespace std;
 
 int main() 
 {
-    
     unsigned int base;
     unsigned int altura;
-
-    cout <<endl <<"Introduce la base del rectangulo: ";
     cin >> base;
-    cout <<endl <<"Introduce la altura del rectangulo: ";
     cin >> altura;
-
-    cout <<endl<<"El area es: "<<(base * altura)/2<<endl;
+    cout<<base*altura;
 
     return 0;
 }
